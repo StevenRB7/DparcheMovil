5ea0e206b96c2808ad22a6945fa1055b7f02916a
kamiloPROO <fotocopiasa100@gmail.com>
2022-09-20 10:39:49 -0500

    melo

--- app/src/main/java/com/kamilo/deparche/Login.java
+++ app/src/main/java/com/kamilo/deparche/Login.java
@@ -34,11 +34,11 @@ public class Login extends AppCompatActivity {
 
     private FirebaseAuth.AuthStateListener mAuthStateListener;
     private Button bntGoogle;
-    TextView TxtOlvide;
+
     int RC_SIGN_IN = 1;
-    Button btningresar;
+
     public static String TAG = "GoogleSignIn", correoString, contraaseniaString;
-    public static EditText correo, contrasenia;
+
 
 
     private FirebaseAuth mAuth;
@@ -52,7 +52,6 @@ public class Login extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        referenciar();
 
         bntGoogle = findViewById(R.id.btnSignIn);
         bntGoogle.setOnClickListener(new View.OnClickListener() {
@@ -68,15 +67,7 @@ public class Login extends AppCompatActivity {
             }
         });
 
-        TxtOlvide = findViewById(R.id.txtOlvideCon);
-        TxtOlvide.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(Login.this, OlvidarContra.class);
-                startActivity(intent);
 
-            }
-        });
 
 
 
@@ -89,28 +80,6 @@ public class Login extends AppCompatActivity {
         mAuth = FirebaseAuth.getInstance();
     }
 
-    private void referenciar() {
-
-        correo = findViewById(R.id.idTxtCorreo);
-        contrasenia = findViewById(R.id.idTxtContrasenia);
-
-        btningresar = findViewById(R.id.button);
-        btningresar.setOnClickListener(new View.OnClickListener() {
-
-            @Override
-            public void onClick(View view) {
-                correoString = correo.getText().toString();
-                contraaseniaString = contrasenia.getText().toString();
-                if (!(validarEmail(correoString)) || !(validarcontrasenas(contraaseniaString))) {
-                    //
-                } else {
-                    Intent intent = new Intent(Login.this, Emociones.class);
-                    startActivity(intent);
-                    finish();
-                }
-            }
-        });
-    }
 
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
@@ -178,60 +147,6 @@ public class Login extends AppCompatActivity {
     private void signIn() {
         Intent signInIntent = mGoogleSignInCliebt.getSignInIntent();
         startActivityForResult(signInIntent, RC_SIGN_IN);
-    }
-
-    private boolean validarEmail(String email) {
-        Pattern pattern = Patterns.EMAIL_ADDRESS;
-        Boolean esValido = true;
-        if (!pattern.matcher(email).find()){
-            correo.setError("Email invalido");
-            esValido = false;
-        } else {
-            esValido = true;
-        }
-        return pattern.matcher(email).matches();
-    }
-
-    public boolean validarcontrasenas(String contrasenas) {
-        Boolean esValido = true;
-        Pattern mayusculas = Pattern.compile("[A-Z]");
-        Pattern minusculas = Pattern.compile("[a-z]");
-        Pattern numeros = Pattern.compile("[0-9]");
-
-        if (!minusculas.matcher(contrasenas).find()) {
-            contrasenia.setError("contraseña invalida");
-            esValido = false;
-        } else {
-            esValido = true;
-        }
-
-        if (!mayusculas.matcher(contrasenas).find()) {
-            contrasenia.setError("contraseña invalida");
-
-            esValido = false;
-        } else {
-            esValido = true;
-
-        }
-        if (!numeros.matcher(contrasenas).find()) {
-            contrasenia.setError("contraseña invalida");
-
-            esValido = false;
-
-        } else {
-
-            esValido = true;
-
-        }
-        if (contrasenas.length() < 8) {
-            contrasenia.setError("contraseña invalida");
-            esValido = false;
-            //charcount.setTextColor(Color.RED);
-        } else {
-            esValido = true;
-            // charcount.setTextColor(Color.GREEN);
-        }
-        return esValido;
     }
 
 
